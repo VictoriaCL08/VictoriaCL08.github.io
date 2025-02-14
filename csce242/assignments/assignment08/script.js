fca72e8d3c986134d48aade596a975fd07ab78a8
VictoriaCL08 <colonlav@email.sc.edu>
2025-02-14 13:45:04 -0500

    assignment 8

--- csce242/assignments/assignment08/script.js
+++ csce242/assignments/assignment08/script.js
@@ -1,3 +1,13 @@
+//Exercise 1 vs exercise 2
+document.getElementById("exercise1").onclick = () => {
+    document.getElementById("travel-section").classList.remove("hidden");
+    document.getElementById("picture-chooser").classList.add("hidden");
+}
+document.getElementById("exercise2").onclick = () => {
+    document.getElementById("travel-section").classList.add("hidden");
+    document.getElementById("picture-chooser").classList.remove("hidden");
+}
+
 
 ////toggle menu
 document.getElementById("toggle-nav").onclick = () => {
@@ -5,3 +15,32 @@ document.getElementById("toggle-nav").onclick = () => {
     document.getElementById("triangle-up").classList.toggle("hidden");
     document.getElementById("triangle-down").classList.toggle("hidden");
 };
+
+/// Transportation page
+document.getElementById("transportation-mode").onkeyup = (event) =>{
+    const travelType = event.currentTarget.value.toLowerCase().trim();
+    document.getElementById("bike").classList.add("hidden");
+    document.getElementById("scooter").classList.add("hidden");
+    document.getElementById("car").classList.add("hidden");
+    if(travelType == "bike"){
+        document.getElementById("bike").classList.remove("hidden");
+    }
+    else if (travelType == "scooter"){
+        document.getElementById("scooter").classList.remove("hidden");   
+    }
+    else if (travelType == "car"){
+        document.getElementById("car").classList.remove("hidden");   
+    }
+};
+
+///  Color Chooser
+document.getElementById("btn-red").onclick = () =>{
+    document.getElementById("heart").style.setProperty("--heart-color", "red");
+};
+document.getElementById("btn-green").onclick = () =>{
+    document.getElementById("heart").style.setProperty("--heart-color", "green");
+};
+document.getElementById("btn-blue").onclick = () =>{
+    document.getElementById("heart").style.setProperty("--heart-color", "blue");
+};
+    
